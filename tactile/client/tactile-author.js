77a5bbac608880f78f1437c19d701d9604eecc3d
alexlockhart7 <alex.lockhart@icfolson.com>
2017-12-19 07:43:54 -0500

    Added paper input and paper checkbox elements based upon the author configuration.

--- tactile/client/tactile-author.js
+++ tactile/client/tactile-author.js
@@ -1,7 +1,10 @@
 import { Element as PolymerElement } from '@polymer/polymer/polymer-element';
 import { ajaxGet } from "./ajax.js";
+import {html, render} from 'lit-html';
 import "@polymer/paper-dialog/paper-dialog";
 import "@polymer/paper-button/paper-button";
+import "@polymer/paper-input/paper-input";
+import "@polymer/paper-checkbox/paper-checkbox";
 
 export default class TactileAuthor extends PolymerElement {
   static get is() {
@@ -20,35 +23,68 @@ export default class TactileAuthor extends PolymerElement {
     };
   }
 
+  _createInputs(component) {
+    var inputs = [ ];
+    component.author.attrs.forEach((input) => {
+      if (input.type === "String") {
+        console.log("STRING");
+        inputs.push(
+          html`<paper-input
+                name=${input.name}
+                label=${input.name}
+                value=${component[input.name]}></paper-input>`);
+      } else if (input.type === "Boolean") {
+        console.log("BOOLEAN");
+        inputs.push(
+          html`<paper-checkbox
+                name=${input.name}
+                checked=${component[input.name]}>${input.name}</paper-checkbox>`);
+      }
+    });
+    return inputs;
+  }
+
+  _createMessage(component) {
+    return html`
+      <h2>${component.compType}</h2>
+      <p>We need to loop over the values in component.author and create paper-input elements
+         based upon their name and type. Finally we need to submit the values to a POST API
+         which is also yet to be created.</p>`;
+  }
+
+  _createButtons() {
+    return html`
+      <div class="buttons">
+        <paper-button dialog-dismiss>Decline</paper-button>
+        <paper-button dialog-confirm autofocus>Accept</paper-button>
+      </div>`;
+  }
+
   openDialog(callback) {
     ajaxGet(this.path+".json", (component) => {
-      // TODO Open a dialog for editing the data based upon component.author
-      // which contains the field list and the rest of that json which contains
-      // the actual properties.
-      console.log(component);
-
       var shadow = this.shadowRoot;
       if (! this.shadowRoot) {
         shadow = this.attachShadow({mode: "open"});
       }
 
-      shadow.innerHTML = `
+      var inputs = this._createInputs(component);
+      var message = this._createMessage(component);
+      var buttons = this._createButtons();
+
+      render(html`
         <paper-dialog modal>
-          <h2>${component.compType}</h2>
-          <p>We need to loop over the values in component.author and create paper-input elements
-             based upon their name and type. Finally we need to submit the values to a POST API
-             which is also yet to be created.</p>
-          <div class="buttons">
-            <paper-button dialog-dismiss>Decline</paper-button>
-            <paper-button dialog-confirm autofocus>Accept</paper-button>
-          </div>
-        </paper-dialog>`;
+          ${message}
+          ${inputs}
+          ${buttons}
+        </paper-dialog>`, shadow);
 
       var paperDialog = shadow.querySelector("paper-dialog")
       paperDialog.open();
-      paperDialog.addEventListener("iron-overlay-closed", callback);
-
-      // TODO Still need a POST API for updating content.json
+      paperDialog.addEventListener("iron-overlay-closed", (e) => {
+        // TODO Inspect the input elements for the values to send over a POST
+        // API for updating content.json
+        callback();
+      });
     });
   }
 }
