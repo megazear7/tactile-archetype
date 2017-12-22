4aa4b6e09814507e75617d7c004ac441b6a76270
alexlockhart7 <alex.lockhart@icfolson.com>
2017-12-22 13:41:34 -0500

    Cleaned up the tactile-author element.

--- tactile/client/tactile-author.js
+++ tactile/client/tactile-author.js
@@ -12,16 +12,100 @@ export default class TactileAuthor extends PolymerElement {
     return "tactile-author";
   }
 
-  constructor() {
-    super();
-  }
-
   static get properties() {
     return {
       path: {
           type: String,
       }
     };
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({mode: "open"});
+  }
+
+  render() {
+    var inputs = ``;
+    var message = ``;
+    var buttons = ``;
+
+    if (this.component) {
+      inputs = this._createInputs(this.component);
+      message = this._createMessage(this.component);
+      buttons = this._createButtons(this.component);
+    }
+
+    render(html`
+    <paper-dialog modal style="min-width: 600px;">
+      ${message}
+      ${inputs}
+      ${buttons}
+    </paper-dialog>
+    `, this.shadowRoot);
+  }
+
+  openDialog(callback) {
+    ajaxGet(this.path+".json", (component) => {
+      this.component = component;
+      this.render();
+      var paperDialog = this.shadowRoot.querySelector("paper-dialog")
+      paperDialog.open();
+      this._attachButtonHandlers();
+      this._attachClosedHandlers(callback);
+    });
+  }
+
+  formValues() {
+    var values = {};
+    this.shadowRoot.querySelectorAll("paper-input").forEach((input) => {
+      if (typeof input.value !== "undefined") {
+        values[input.name] = input.value;
+      } else {
+        values[input.name] = "";
+      }
+    });
+    this.shadowRoot.querySelectorAll("paper-checkbox").forEach((input) => {
+      if (typeof input.value !== "undefined") {
+        console.log(input.active);
+        values[input.name] = input.active;
+      } else {
+        values[input.name] = false;
+      }
+    });
+    return values;
+  }
+
+  _attachClosedHandlers(callback) {
+    var paperDialog = this.shadowRoot.querySelector("paper-dialog")
+
+    var dialogClosed = (event) => {
+      if (event.detail.confirmed) {
+        ajaxPost(this.path, this.formValues());
+      }
+      paperDialog.removeEventListener("iron-overlay-closed", dialogClosed);
+      callback(event.detail.confirmed);
+    }
+    
+    paperDialog.addEventListener("iron-overlay-closed", dialogClosed);
+  }
+
+  _attachButtonHandlers() {
+    var paperDialog = this.shadowRoot.querySelector("paper-dialog")
+
+    Array.from(paperDialog.querySelectorAll("paper-button.tactile-add")).forEach(button => {
+      button.addEventListener("click", (e) => {
+        var path = this.path+"/"+e.target.dataset.path;
+        var template = JSON.parse(e.target.dataset.template);
+        ajaxPut(path, template, callback);
+      });
+    });
+
+    Array.from(paperDialog.querySelectorAll("paper-button.tactile-delete")).forEach(button => {
+      button.addEventListener("click", (e) => {
+        ajaxDelete(this.path, callback);
+      });
+    });
   }
 
   _createInputs(component) {
@@ -79,72 +163,6 @@ export default class TactileAuthor extends PolymerElement {
         <paper-button dialog-dismiss>Decline</paper-button>
         <paper-button dialog-confirm autofocus>Accept</paper-button>
       </div>`;
-  }
-
-  openDialog(callback) {
-    ajaxGet(this.path+".json", (component) => {
-      var shadow = this.shadowRoot;
-      if (! this.shadowRoot) {
-        shadow = this.attachShadow({mode: "open"});
-      }
-
-      var inputs = this._createInputs(component);
-      var message = this._createMessage(component);
-      var buttons = this._createButtons(component);
-
-      render(html`
-        <paper-dialog modal style="min-width: 600px;">
-          ${message}
-          ${inputs}
-          ${buttons}
-        </paper-dialog>`, shadow);
-
-      var paperDialog = shadow.querySelector("paper-dialog")
-      paperDialog.open();
-
-      Array.from(paperDialog.querySelectorAll("paper-button.tactile-add")).forEach(button => {
-        button.addEventListener("click", (e) => {
-          var path = this.path+"/"+e.target.dataset.path;
-          var template = JSON.parse(e.target.dataset.template);
-          ajaxPut(path, template, callback);
-        });
-      });
-
-      Array.from(paperDialog.querySelectorAll("paper-button.tactile-delete")).forEach(button => {
-        button.addEventListener("click", (e) => {
-          ajaxDelete(this.path, callback);
-        });
-      });
-
-      var ironOverlayClosedHandler = (e) => {
-        // TODO Inspect the input elements for the values to send over a POST
-        // API for updating content.json
-        if (e.detail.confirmed) {
-          var newValues = {};
-          shadow.querySelectorAll("paper-input").forEach((input) => {
-            if (typeof input.value !== "undefined") {
-              newValues[input.name] = input.value;
-            } else {
-              newValues[input.name] = "";
-            }
-          });
-          shadow.querySelectorAll("paper-checkbox").forEach((input) => {
-            if (typeof input.value !== "undefined") {
-              console.log(input.active);
-              newValues[input.name] = input.active;
-            } else {
-              newValues[input.name] = false;
-            }
-          });
-          ajaxPost(this.path, newValues);
-        }
-
-        paperDialog.removeEventListener("iron-overlay-closed", ironOverlayClosedHandler);
-        callback(e.detail.confirmed);
-      }
-
-      paperDialog.addEventListener("iron-overlay-closed", ironOverlayClosedHandler);
-    });
   }
 }
 
