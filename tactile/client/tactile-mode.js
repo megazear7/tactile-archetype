5be062477a5588f7a835889608232f508c47ac87
alexlockhart7 <alex.lockhart@icfolson.com>
2017-12-23 07:39:45 -0500

    Moved the author.js functionality into tactile-mode and refactored it to make it much cleaner.

--- tactile/client/tactile-mode.js
+++ tactile/client/tactile-mode.js
@@ -17,23 +17,56 @@ export default class TactileMode extends PolymerElement {
 
     this.toggle.addEventListener("iron-change", () => {
       if (this.toggle.checked) {
+        this.switchToEdit();
         this.dispatchEvent(new CustomEvent('switched-to-edit'));
       } else {
+        this.switchToPublish();
         this.dispatchEvent(new CustomEvent('switched-to-publish'));
       }
     });
+
+    var mode = window.sessionStorage.getItem("tactile-mode");
+    if (mode === "edit") {
+      this.switchToEdit();
+    } else {
+      this.switchToPublish();
+    }
   }
 
   switchToEdit() {
     this.toggle.checked = true;
+    document.addEventListener("click", TactileMode.watchForComponentClicks);
+    window.sessionStorage.setItem("tactile-mode", "edit");
   }
 
   switchToPublish() {
     this.toggle.checked = false;
+    document.removeEventListener("click", TactileMode.watchForComponentClicks);
+    window.sessionStorage.setItem("tactile-mode", "publish");
   }
 
   get mode() {
     return this.toggle.checked;
+  }
+
+  static watchForComponentClicks(event) {
+    // This stops user from interacting with page in author mode
+    event.stopPropagation();
+    event.preventDefault();
+
+    var tactileEditable = event.target.closest("tactile-editable");
+    if (tactileEditable) {
+      document.removeEventListener("click", TactileMode.watchForComponentClicks);
+
+      tactileEditable.openDialog(function(requiresRefresh = true) {
+        document.addEventListener("click", TactileMode.watchForComponentClicks);
+
+        // TODO just reload the component, not the entire page.
+        if (requiresRefresh) {
+          window.location.reload();
+        }
+      });
+    }
   }
 }
 
