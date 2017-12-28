3c093a9e575f60d7afc7b724da5119745ef4bc63
alexlockhart7 <alex.lockhart@icfolson.com>
2017-12-27 21:53:19 -0500

    test commit for heroku

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const componentModels = require('./components/components.js')
 const authorModels = require('./components/author.js')
 const PORT = process.env.PORT || 3000
 
+
 tactileClerk.run(PORT, componentModels, authorModels);
