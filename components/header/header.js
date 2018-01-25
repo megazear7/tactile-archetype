c9ca68139a21a26a84302eec7f766a9d49f1c967
alexlockhart7 <alex.lockhart@icfolson.com>
2018-01-25 15:05:04 -0500

    Updated header component to include a link

--- components/header/header.js
+++ components/header/header.js
@@ -1,20 +1,14 @@
 module.exports = function(component) {
-  /*links = [{
-      href: node.page.home.path,
-      title: node.page.home.title,
-      isCurrentPage: node.page.home.path === node.page.path
-  }];
+  links = [{
+    href: "/",
+    title: "Home",
+    isCurrentPage: false
+  }]
 
-  node.page.home.children.forEach(function(child) {
-    links.push({
-      href: child.path,
-      title: child.title,
-      isCurrentPage: child.path === node.page.path
-    });
-  });*/
+  // TODO Add child links
 
   return {
-    //links: links
+    links: links,
     title: "Header"
   }
 };
